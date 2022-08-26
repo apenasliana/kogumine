c7de9885523f5e45981e8b339f32545660aff5d7
apenasliana <miresukali@gmail.com>
2022-08-26 11:58:31 -0300

    importando o backup do DB para uso externo

---
+++ server/mySql-DatabaseBackup/dump/kogumine-db_carta.sql
@@ -0,0 +1,64 @@
+-- MariaDB dump 10.19  Distrib 10.8.3-MariaDB, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: kogumine-db
+-- ------------------------------------------------------
+-- Server version	10.8.3-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `carta`
+--
+
+DROP TABLE IF EXISTS `carta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `carta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `raridade` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `nome_UNIQUE` (`nome`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carta`
+--
+
+LOCK TABLES `carta` WRITE;
+/*!40000 ALTER TABLE `carta` DISABLE KEYS */;
+INSERT INTO `carta` VALUES
+(10,'Fog','common',0.26),
+(11,'Opt','common',0.10),
+(12,'Faerie Miscreant','common',0.00),
+(13,'Teferi\'s Moat','special',1.09),
+(14,'Upwelling','rare',1.71),
+(15,'Spine of Ish Sah','rare',0.74),
+(16,'Wolfwillow Haven','uncommon',0.38),
+(17,'Creeping Trailblazer','uncommon',0.14),
+(18,'Midnight Clock','rare',0.42);
+/*!40000 ALTER TABLE `carta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-26 11:56:00
